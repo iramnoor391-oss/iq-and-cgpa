264fce7bb2a08c589168a497a126f459c3bb3f63
iramnoor391-oss <iramnoor391@gmail.com>
2026-07-24 21:14:26 +0530

    upload IQ and CGPA project

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle as pkl
+
+with open('mlp_slp_lpa_model.pkl','rb') as f:
+    model = pkl.load(f)
+with open('scaler.pkl','rb') as f:
+    sc = pkl.load(f)
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+
+def lpa_predictor():
+    if request.method=='POST':
+        iq = float(request.form['iq'])
+        cgpa = float(request.form['cgpa'])
+        iq_scaled = sc.transform([[iq, cgpa]])
+        result = model.predict(iq_scaled)[0]
+        return render_template('index.html', prediction=f"{result} LPA")
+    else:
+        return render_template('index.html', prediction=None)
+    
+if __name__=='__main__':
+    app.run(debug=True)
